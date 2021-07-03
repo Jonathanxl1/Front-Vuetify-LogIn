dfcb7ffd23bce33b1bbd6e06a6a1b55665ad30a3
Jonathan A <jonathanxl1@hotmail.com>
2021-07-03 10:59:58 -0500

    Configuracion Basica Theme mobiera

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="dark" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Mobiera Logo"
+          class="shrink mr-2"
+          contain
+          src="../src/assets/logo.png"
+          transition="scale-transition"
+          width="150"
+        />
+      </div>
+    </v-app-bar>
+
+    <v-main> </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
